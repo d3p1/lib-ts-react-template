8c71faaca4930061ab32228b39b9a9b925770081
C. M. de Picciotto <d3p1@d3p1.dev>
2025-12-28 14:11:17 +0000

    chore: improve dev page [#1]

--- dev/js/react/app.tsx
+++ dev/js/react/app.tsx
@@ -3,7 +3,7 @@
  * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
  */
 import {createRoot} from 'react-dom/client'
-import {CanvasMotionCircle} from '../../../src/react'
+import {CanvasMotionCircle} from '@d3p1/lib-ts-react-template/react'
 
 createRoot(document.getElementById('react-root') as HTMLDivElement).render(
   <>
