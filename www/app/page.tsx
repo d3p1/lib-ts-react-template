2bc1fae499c7d83584425f5c4f2f74e3266f7b5e
C. M. de Picciotto <d3p1@d3p1.dev>
2025-12-28 01:11:19 +0000

    feat: improve demo page [#1]

--- www/app/page.tsx
+++ www/app/page.tsx
@@ -2,6 +2,15 @@
  * @description Home page
  * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
  */
+'use client'
+
+import {CanvasMotionCircle} from '../../src/react'
+
 export default function Page() {
-  return null
+  return (
+    <>
+      <h2>React Lib</h2>
+      <CanvasMotionCircle />
+    </>
+  )
 }
