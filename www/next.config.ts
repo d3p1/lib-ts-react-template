20ac9df5d8455b631d62b611e3095eb710a0e867
C. M. de Picciotto <d3p1@d3p1.dev>
2025-12-31 22:24:25 +0000

    chore: adjust demo page build to avoid root directory recognition warning [#1]

--- www/next.config.ts
+++ www/next.config.ts
@@ -3,6 +3,9 @@ import type {NextConfig} from 'next'
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/lib-ts-react-template',
+  turbopack: {
+    root: __dirname,
+  },
 }
 
 export default nextConfig
